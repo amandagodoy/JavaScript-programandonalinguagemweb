7651a0226e19548e1ed2c429b63f0e240f996c2c
amandagodoy <amandagodoylima@gmail.com>
2020-11-20 16:12:14 -0300

    commit

---
+++ js/principal.js
@@ -0,0 +1,20 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura; // 100 / 2.0 x 2.0 = 100 / 4 =>>>>>>> 25
+
+console.log(imc);
+
+/*console.log(paciente); // tr
+console.log(tdPeso); // td que tem o peso
+console.log(peso); //Obter 100
+console.log(tdAltura); 
+console.log(altura); */
