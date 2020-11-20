12c8fae8f9f869e09fada6fd63529e7c4ddb97e1
amandagodoy <amandagodoylima@gmail.com>
2020-11-20 18:11:44 -0300

    commit

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,5 @@
 var titulo = document.getElementsByClassName("titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-console.log(paciente);
 var paciente = document.getElementById("primeiro-paciente");
-
+console.log(paciente);
